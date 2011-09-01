4d59c82f0955ffb20164c07f737a6c902271b8e5
Wojtek <wojciech.g.krysiak@gmail.com>
2011-09-01 11:16:18 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rekrutacja::Application.config.secret_token = '8510e9ee8775baef2d5276f2af77151196ebd94a5d51597807f08a706751150c04253d77f97c5c6448c24bf9e20823cc1f2e6fa1f94eaf36d79d3d5eabb4d185'
